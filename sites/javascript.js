932171904ec125cc14f08aab761455798298c493
Laluhat <82870623+Laluhat@users.noreply.github.com>
2021-12-09 13:15:20 +0100

    JAP

--- sites/javascript.js
+++ sites/javascript.js
@@ -1,4 +1,4 @@
-//. Taschenrechner
+// Taschenrechner
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
 const display = calculator.querySelector('.calculator__display')
