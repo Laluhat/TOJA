e28d50cb378cabd1176cb087b2ecc1251967ce08
Laluhat <82870623+Laluhat@users.noreply.github.com>
2022-01-04 10:49:33 +0100

    approved

--- sites/data.js
+++ sites/data.js
@@ -19,38 +19,3 @@ let customer = {
     }
 }
 
-
-//identification
-let htmlObj = document.getElementById("identification");
-htmlObj.innerHTML =
-    `id: ${customer.id}<br>`+
-    `Name: ${customer.firstname}<br>`+
-    `Vorname: ${customer.lastname}<br>`+
-    `Geschlcht: ${customer.gender}<br>`;
-
-//Location
-htmlObj = document.getElementById("location");
-htmlObj.innerHTML =
-    `Land: ${customer.location.country}<br>`+
-    `Stadt: ${customer.location.city}<br>`+
-    `Strasse: ${customer.location.street}`;
-
-//product - coffe
-htmlObj = document.getElementById("product");
-customer.product.coffe.forEach(coffeid => {
-    htmlObj.innerHTML += `Coffe: ${coffeid}<br>`;
-})
-
-//restaurant or takeaway
-
-let Rot = "Takeawy";
-if (customer.restaurantortakeaway.restaurant){
-    Rot = "Restaurant";
-}
-
-htmlObj = document.getElementById("aprove");
-let restaurantliste = customer.restaurantortakeaway.aprove;
-for (let i=0;i<restaurantliste.length;i++){
-    htmlObj.innerHTML += `${Rot}: ${restaurantliste[i]}<br>`;
-}
-
